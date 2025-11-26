d5cfcb424d2b108fba40207c27988de472361df4
Артем Попов <popovartemp@gmail.com>
2025-11-26 09:22:38 +0300

    Добавьте файлы проекта.

---
+++ TeamProject/Person.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamProject
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private DateTime birthDate;
+        public Person(string firstName, string lastName, DateTime birthDate)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.birthDate = birthDate;
+        }
+        public Person()
+        {
+            firstName = "Нет имени";
+            lastName = "Нет фамилии";
+            birthDate = new DateTime(2025, 1, 1);
+        }
+        public string FirstName
+        {
+            get 
+            { 
+                return firstName; 
+            }
+            set 
+            { 
+                firstName = value; 
+            }
+        }
+        public string LastName
+        {
+            get 
+            { 
+                return lastName; 
+            }
+            set 
+            { 
+                lastName = value; 
+            }
+        }
+        public DateTime BirthDate
+        {
+            get 
+            { 
+                return birthDate; 
+            }
+            set 
+            { 
+                birthDate = value; 
+            }
+        }
+        public int BirthYear
+        {
+            get 
+            { 
+                return birthDate.Year; 
+            }
+            set 
+            { 
+                birthDate = new DateTime(value, birthDate.Month, birthDate.Day); 
+            }
+        }
+        public override string ToString()
+        {
+            return "Имя " + firstName + " Фамилия " + lastName + " Дата рождения " + birthDate.ToShortDateString();
+        }
+        public virtual string ToShortString()
+        {
+            return "Имя " + firstName + " Фамилия " + lastName;
+        }
+    }
+}
---
+++ TeamProject/Program.cs
@@ -0,0 +1,44 @@
+﻿using TeamProject;
+Console.WriteLine("Введите количество строк:");
+int nrow = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов:");
+int ncolumn = int.Parse(Console.ReadLine());
+
+Person[] mas1 = new Person[nrow * ncolumn];
+Person[,] mas2 = new Person[nrow, ncolumn];
+
+for (int i = 0; i < mas1.Length; i++)
+{
+    mas1[i] = new Person();
+}
+
+for (int i = 0; i < nrow; i++)
+{
+    for (int j = 0; j < ncolumn; j++)
+    {
+        mas2[i, j] = new Person();
+    }
+}
+
+int start = Environment.TickCount;
+for (int i = 0; i < mas1.Length; i++)
+{
+    mas1[i].BirthYear = 2025;
+}
+int end = Environment.TickCount;
+int oneDimTime = end - start;
+
+start = Environment.TickCount;
+for (int i = 0; i < nrow; i++)
+{
+    for (int j = 0; j < ncolumn; j++)
+    {
+        mas2[i, j].BirthYear = 2025;
+    }
+}
+end = Environment.TickCount;
+int twoDimRectTime = end - start;
+
+Console.WriteLine("Количество строк " + nrow + " Количество столбцов " + ncolumn);
+Console.WriteLine("Время выполнения операций mas1 " + oneDimTime + " мс");
+Console.WriteLine("Время выполнения операций mas2 " + twoDimRectTime + " мс");
