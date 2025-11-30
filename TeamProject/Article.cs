c38ace0f47e646398cff0e7fcfcd9815d07e6165
Артем Попов <popovartemp@gmail.com>
2025-11-30 18:36:52 +0300

    Добавил перечисление и класс Article

---
+++ TeamProject/Article.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamProject
+{
+    public enum Frequency
+    {
+        Weekly,
+        Monthly,
+        Yearly
+    }
+    public class Article
+    {
+        private Person author;
+        private string title;
+        private double rating;
+        public Person Author
+        {
+            get 
+            { 
+                return author; 
+            }
+            set 
+            { 
+                author = value; 
+            }
+        }
+        public string Title
+        {
+            get 
+            { 
+                return title; 
+            }
+            set 
+            { 
+                title = value; 
+            }
+        }
+        public double Rating
+        {
+            get 
+            { 
+                return rating; 
+            }
+            set
+            {
+                rating = value; 
+            }
+        }
+        public Article(Person author, string title, double rating)
+        {
+            this.author = author;
+            this.title = title;
+            this.rating = rating;
+        }
+        public Article()
+        {
+            author = new Person();
+            title = "Без названия";
+            rating = 0.0;
+        }
+        public override string ToString()
+        {
+            return "Автор " + author.ToShortString() + " название " + title + " рейтинг " + rating;
+        }
+    }
+}
